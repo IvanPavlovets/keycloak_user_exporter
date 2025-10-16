068edd6dd284ad07edb7e94335e529ced97f3e2a
IvanPavlovets <ivan150287@gmail.com>
2025-10-16 12:20:32 +0700

    init

---
+++ src/test/java/com/example/keycloak_user_exporter/KeycloakUserExporterAppTests.java
@@ -0,0 +1,13 @@
+package com.example.keycloak_user_exporter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloakUserExporterAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
